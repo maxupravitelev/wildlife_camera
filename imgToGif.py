c7603e1ce81edb63e0d48d6fa53060d7c7a2d1e5
Max Upravitelev <max@upravitelev.de>
2020-11-26 15:26:41 +0100

    combine jpeg folder to gif

--- imgToGif.py
+++ imgToGif.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
+import glob 
 
-images = []
 
 # def imgToGif():
 #     for i in range(1, 9):
@@ -10,10 +10,17 @@ images = []
 #                    save_all=True, append_images=images[1:], optimize=False, duration=80, loop=0)
 
 def imgToGif(folderCount):
-    for i in range(1, 9):
-        img = Image.open("images/image"+str(i)+".jpg")
-        images.append(img)
-    images[0].save('out.gif',
+    images = []
+
+    for img in glob.glob("gifs/images" + str(folderCount) + "/" + "*.jpg"):
+        newJpg = Image.open(img)
+        images.append(newJpg)
+        print(img)
+
+    # for i in range(1, len(images)):
+        #img = Image.open("images/image"+str(i)+".jpg")
+    #    images.append(img)
+    images[0].save('gifs/out'+ str(folderCount) + '.gif',
                    save_all=True, append_images=images[1:], optimize=False, duration=80, loop=0)
 
-imgToGif()
+imgToGif(0)
