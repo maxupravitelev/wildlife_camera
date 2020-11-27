35c82b5a245bacaae4d7357c56f4e1bf8a7239b7
Max Upravitelev <max@upravitelev.de>
2020-11-27 15:29:36 +0100

    clean up code

--- webstreaming.py
+++ webstreaming.py
@@ -36,29 +36,8 @@ cap = cv2.VideoCapture(0)
 
 motion_detected = False
 
-# Define the codec and create VideoWriter object
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-
 count = 0
 folderCount = 0
-
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#     if ret==True and motion_detected==True:
-#         frame = cv2.flip(frame,0)
-#         # cv2.imwrite('image'+str(count)+'.jpg',frame)
-
-
-#         # write the flipped frame
-#         out.write(frame)
-
-# #        cv2.imshow('frame',frame)
-# #        if cv2.waitKey(1) & 0xFF == ord('q'):
-# #            break
-#     else:
-#         break
-
 
 
 @app.route("/")
