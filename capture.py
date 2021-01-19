53b5db125a1b7f7268922e73df50fd185702f58e
Max Upravitelev <max@upravitelev.de>
2021-01-19 09:08:23 +0100

    add aprox. FPS test mode

--- capture.py
+++ capture.py
@@ -6,15 +6,21 @@ import numpy as np
 
 from functions.cam import VideoStream
 
-# from functions.PiCam import PiCam 
+from functions.PiCam import PiCam 
 
 from functions.file_writer import File_writer
 
 ## mode selection
 debug_mode = False
-enable_timer = True
+enable_timer = False
+approx_fps = True
 
-cap = VideoStream(src=0).start()
+## init capture
+frame_width = 1024
+frame_height = 768
+
+# cap = VideoStream(src=0).start()
+cap = PiCam(resolution=(frame_width,frame_height)).start()
 
 time.sleep(2.0)
 
@@ -25,60 +31,64 @@ frame = cap.read()
 # get frame size from first frame making
 print("Frame resolution: " + str(frame.shape))
 
+if approx_fps == True:
 
+    start = time.time()
 
-start = time.time()
+    frames = 0
+    time_limit = 60
 
-frames = 0
-time_limit = 60
+    while frames < time_limit:
+        frame = cap.read()
+        if cap.same_frame == False:
+            frames += 1
 
-while time_limit < time_limit:
-    frame = cap.read()
-    if cap.same_frame == False:
-        frames += 1
+    end = time.time()
 
-end = time.time()
+    print("approx. FPS: " + str(time_limit/(end-start)))
 
-print("FPS: " + str(time_limit/(end-start)))
+    cap.stop()
 
-
-if debug_mode == False:
-    File_writer = File_writer(mode="gif").start()
-    File_writer.motion_detected = True
-
-if enable_timer == True:
-    timer2 = time.time()
-    timer2 = time.time()
-
-frame = cap.read()
-
-while True:
-    last_frame = frame.copy()
-    frame = cap.read()
-
+if approx_fps == False:
 
     if debug_mode == False:
-        File_writer.frame = frame.copy()
-
-    
-    if debug_mode == True:
-        # view color frame
-        cv2.imshow("video feed", frame)
+        File_writer = File_writer(mode="gif").start()
+        File_writer.motion_detected = True
 
     if enable_timer == True:
+        timer2 = time.time()
+        timer2 = time.time()
 
-        timer1 = time.time()
-    
-        if (cap.same_frame == False):
-            
-            print("FPS: " + str(1/((timer1-timer2))))
-            timer2 = time.time()
+    frame = cap.read()
 
-    # loop breaking condition
-    key = cv2.waitKey(1) & 0xFF
 
-    if key == ord("x"):
-        break
+    while True:
+        last_frame = frame.copy()
+        frame = cap.read()
+
+
+        if debug_mode == False:
+            File_writer.frame = frame.copy()
+
+        
+        if debug_mode == True:
+            # view color frame
+            cv2.imshow("video feed", frame)
+
+        if enable_timer == True:
+
+            timer1 = time.time()
+        
+            if (cap.same_frame == False):
+                
+                print("FPS: " + str(1/((timer1-timer2))))
+                timer2 = time.time()
+
+        # loop breaking condition
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("x"):
+            break
 
 # clean up
 cap.stop()
